32af58eac2d1c4e622802dc28aadd3b2697980a5
David Filat <david.filat@eurasiaprecept.org>
2019-11-24 10:54:32 +0200

    Refactored database paths

--- database.js
+++ database.js
@@ -1,21 +1,22 @@
-var fs = require('fs')
+const fs = require('fs')
+const path = require('path')
+
+const usersDbPath = path.join(__dirname, '/db-users.json')
 var databaseJSON = {
   getUsers: function () {
     return new Promise(function (resolve, reject) {
-      fs.readFile(__dirname + '/db-users.json', 'utf8', function (
+      fs.readFile(usersDbPath, 'utf8', function (
         err,
         content
       ) {
         if (err) return reject(err)
-
         resolve(JSON.parse(content))
       })
     })
   },
   setUsers: function (users) {
     return new Promise(function (resolve, reject) {
-      fs.writeFile(
-        __dirname + '/db-users.json',
+      fs.writeFile(usersDbPath,
         JSON.stringify(users, null, '\t'),
         function (err) {
           if (err) return reject(err)
--- textbase.js
+++ textbase.js
@@ -1,8 +1,11 @@
-var fs = require('fs')
-var textJSON = {
+const fs = require('fs')
+const path = require('path')
+
+const textDbPath = path.join(__dirname, '/db-text.json')
+const textJSON = {
   getText: function () {
     return new Promise(function (resolve, reject) {
-      fs.readFile(__dirname + '/db-text.json', 'utf8', function (err, content) {
+      fs.readFile(textDbPath, 'utf8', function (err, content) {
         if (err) return reject(err)
 
         resolve(JSON.parse(content))
@@ -11,14 +14,12 @@ var textJSON = {
   },
   setText: function (texts) {
     return new Promise(function (resolve, reject) {
-      fs.writeFile(
-        __dirname + '/db-text.json',
-        JSON.stringify(texts, null, '\t'),
-        function (err) {
-          if (err) return reject(err)
-          resolve()
-        }
-      )
+      fs.writeFile(textDbPath, JSON.stringify(texts, null, '\t'), function (
+        err
+      ) {
+        if (err) return reject(err)
+        resolve()
+      })
     })
   }
 }
